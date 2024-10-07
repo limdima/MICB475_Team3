aa07200e1d1e23f79b67a6466b58ee87fa53fbb6
Natalia <121647379+nb488@users.noreply.github.com>
2024-10-07 12:55:32 -0700

    Update trim_meta_rscript.R
    - added more filtering (diet, eating disorder)
    - retrieved samples for ms types
    - retrieved manifest file

--- trim_meta_rscript.R
+++ trim_meta_rscript.R
@@ -2,8 +2,10 @@ library(dplyr)
 library(tidyverse)
 library(readxl)
 # install.packages("readxl")
+set.seed(1234) # set random seed
 
 meta_untrimmed <- read_excel("ms_metadata.xlsx")
+manifest_untrimmed <- read.delim("ms_manifest.tsv", sep="\t")
 
 # remove duplicate rows
 
@@ -11,11 +13,19 @@ meta <- distinct(meta_untrimmed)
   
 # filter out probiotics, (etc.), and also any unpaired samples after the filtering
 
-filter_meta <- filter(meta, probiotics == 0) %>%
-# add more filters here for variables we don't need  # filter() %>%
+filter_meta <- meta %>%
+  filter(probiotics == 0) %>%
+  filter(diet_no_special_needs == 1) %>%
+  filter(eating_disorder == 0) %>%
   
   group_by(`household`) %>% # samples are paired by household, one MS and one control
   filter(n() == 2) %>% # filters out any rows that do not contain two rows per household
   ungroup()
+
+# get spms, rrms, and ppms samples
+
+SPMS_filtered <- filter(filter_meta, disease_course=="SPMS") 
+RRMS_filtered <- filter(filter_meta, disease_course=="RRMS")
+PPMS_filtered <- filter(filter_meta, disease_course=="PPMS")
 
 write.csv(filter_meta, "meta_ms", row.names = FALSE)
