1bb9581980e721ba9b1a7963693903ebaee713a9
Natalia <121647379+nb488@users.noreply.github.com>
2024-10-26 01:43:43 -0700

    uploaded ms_phyloseq_object script
    Note: Used files from ms_export on server (with taxonomy, feature table, and rooted tree files) and meta_ms.csv acquired from trim_meta_rscript.R file (Github)

---
+++ ms_phyloseq_object.R
@@ -0,0 +1,42 @@
+# Create phyloseq object
+
+# Load in packages:
+library(phyloseq)
+library(ape)
+library(tidyverse)
+
+# Load in the ms metadata,  feature table, taxonomy file, and rooted tree
+metafp <- "meta_ms.csv"
+meta <- read_delim(metafp, delim=",")
+
+otufp <- "ms_export/ms-table.txt"
+otu <- read_delim(file =otufp, delim="\t", skip=1)
+
+taxfp <- "ms_export/ms-taxonomy.tsv"
+tax <- read_delim(taxfp, delim="\t")
+
+phylotreefp <- "ms_export/ms-tree.nwk"
+phylotree <- read.tree(phylotreefp)
+
+# Adjust files to be read into a phyloseq object. Make the phyloseq object.
+# feature-table;
+otu_mat <- as.matrix(otu[,-1])
+rownames(otu_mat) <- otu$"#OTU ID"
+OTU <- otu_table(otu_mat, taxa_are_rows = TRUE)
+
+# metadata:
+meta_df <- as.data.frame(meta[,-1])
+rownames(meta_df) <- meta$"sample-id"
+META <- sample_data(meta_df)
+
+# taxonomy file:
+tax_mat <- tax %>%
+  select(-Confidence) %>%
+  separate(col=Taxon, sep="; ", into=c("Domain", "Phylum", "Class", "Order", "Family", "Genus", "Species")) %>%
+  as.matrix()
+tax_mat <- tax_mat[,-1]
+rownames(tax_mat) <- tax$"Feature ID"
+TAX <- tax_table(tax_mat)
+
+# phyloseq object:
+ms_phyloseq <- phyloseq(OTU, META, TAX, phylotree)
