14aae7c8de341e928661cd9fc9c82c84c179933a
Andrew Huang <you@example.com>
2024-10-29 00:09:28 -0700

    Core microbiome analysis. Looks weird.. Set 0 detection and prevelence to be least stringent as possible but no unique taxa in SPMS/PPMS. Will try again later with new phyloseq object (pre-filtered) and see if that makes a difference. Maybe there are 0 values that are messing with it.

--- Experimental Aim 2/CoreMicrobiome.R
+++ Experimental Aim 2/CoreMicrobiome.R
@@ -5,4 +5,29 @@ library(microbiome)
 library(ggVennDiagram)
 
 # load the phyloseq object
-load("../phyloseq/")
+load("phyloseq/ms_phyloseq.RData")
+
+# Convert object to relative abundance
+phyloseq_rel <- transform_sample_counts(ms_phyloseq, fun = function(x) x/sum(x))
+
+
+### Subset dataset into treatment and control groups
+RRMS_stat <- subset_samples(phyloseq_rel, disease_course == "RRMS")
+SPMS_stat <- subset_samples(phyloseq_rel, disease_course == "SPMS")
+PPMS_stat<- subset_samples(phyloseq_rel, disease_course == "SPMS")
+
+### Set a prevalence threshold and abundance threshold
+RRMS_ASVs <- core_members(RRMS_stat, detection=0, prevalence = 0)
+SPMS_ASVs <- core_members(SPMS_stat, detection=0, prevalence = 0)
+PPMS_ASVs <- core_members(PPMS_stat, detection=0, prevalence = 0)
+# Keep all ASVs including rare ASVs (detection = 0% relative abundance)
+# Find ASVs present in any samples (prevalence = 0), trying to tease out what is unique to each group
+
+### Make a Venn-diagram
+venn_pd <- ggVennDiagram(x=list(RRMS = RRMS_ASVs, SPMS = SPMS_ASVs, PPMS = PPMS_ASVs)) +
+  scale_x_continuous(expand = expansion(mult = .2)) # this line helps fit the labels into the plot border
+
+venn_pd
+
+### save venn diagram
+ggsave("venn_pd_vs_healthy.png", plot = venn_pd)
