24a60d9b1346a7e71b7fa7ec1ad721f3b029dd1c
Andrew Huang <you@example.com>
2024-10-28 23:26:23 -0700

    Changed the way meta and otu is filtered. Filtered out confounders: probiotics, diet, and eating disorders from metadata and matched it to the corresponding ASV (via sample-id and semi_join) from the OTU table. Similarly, we are not interested in OTUs that are not relevent (no matching metadata) which have been removed from the OTU table based on what is in metadata. This leaves us with 515 samples in total for downstream analysis.
    The table and metadata are saved as RData files, so that they may be put into the phyloseq object easily.

--- phyloseq/ms_trimming_metadata_script.R
+++ phyloseq/ms_trimming_metadata_script.R
@@ -12,18 +12,44 @@ otu <- read_delim(file =otufp, delim="\t", skip=1)
 metafp <- "ms_export/corrected_ms_metadata.tsv"
 meta <- read_delim(metafp, delim="\t")
 
-# transpose the otu object so the rows become columns and vice versa
+# transpose the otu object so the rows become columns and vice versa - this is done so it can be semi_joined against meta later on
 
-otu_transposed <- as.data.frame(t(otu))
-otu_transposed <- cbind("sample-id" = rownames(otu_transposed), otu_transposed)
-colnames(otu_transposed) <- otu_transposed[1,]
-colnames(otu_transposed)[1] <- "sample-id"
-otu_transposed <- otu_transposed[-1,]
+otu_transposed <- as.data.frame(t(otu[, -1]))    # transposes the data but excludes the first column as that will be col names
+colnames(otu_transposed) <- otu[[1]]     # first column in the original otu table is for the OTU IDs, now as the new col names for the transposed table
+otu_transposed <- cbind("sample-id" = rownames(otu_transposed), otu_transposed)   # makes a new column "sample-id" with the original header names from otu, for downstream joining
+rownames(otu_transposed) <- NULL   # removes the indexing row names
 
-# right_join the metadata table and the otu table to filter out unwanted metadata columns:
 
-combined <- right_join(meta, otu_transposed)
+# filter the metadata to get rid of variables that we deemed as potential confounders
+# We think mainly probiotics, diet, and eating disorders will contribute a larger confounding effect on the gut microbiome
 
-# separate the dataframes:
+filter_meta <- meta %>%
+  filter(probiotics == 0) %>%        
+  filter(diet_no_special_needs == 1) %>%
+  filter(eating_disorder == 0) %>%
+  
+  group_by(`household`) %>% # samples are paired by household, one MS and one control
+  filter(n() == 2) %>% # filters out any rows that do not contain two rows per household
+  ungroup()
 
-metadata_trimmed <- combined[, 1:60]
+# after filtering there is less samples in the metadata (522) than OTUs (915)
+# semi_join the metadata table and the otu table to filter out samples that we won't be including:
+# semi_join will keep only the rows in meta with a match in the otu table, without actually merging the two dataframes
+
+metadata_trimmed <- semi_join(filter_meta, otu_transposed, by = 'sample-id')
+# returns 515 samples, meaning some metadata without corresponding OTUs were also removed
+
+otu_filter <- semi_join(otu_transposed, filter_meta, by = 'sample-id')
+# returns the same corresponding 515 samples as in the metadata
+
+
+# re-transpose the otu table so that it is ready to be put into a phyloseq object, similar to before
+otu_trimmed <- as.data.frame(t(otu_filter))
+colnames(otu_trimmed) <- otu_filter[[1]]
+otu_trimmed <- cbind("#OTU ID" = rownames(otu_trimmed), otu_trimmed)   # makes a new column "#OTU ID" as in the original OTU table
+rownames(otu_trimmed) <- NULL
+
+# metadata and otu table both now have 515 samples each after filtering for the variables we will include for analysis
+
+save(metadata_trimmed, file = "metadata_trimmed")
+save(otu_trimmed, file = "otu_trimmed")
