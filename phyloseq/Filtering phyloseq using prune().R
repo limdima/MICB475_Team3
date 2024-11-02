fc384fea3951ef32b04a637d4f455e91fcb01334
limdima <dima.limswims@gmail.com>
2024-11-02 11:44:24 -0700

    Add files via upload

---
+++ phyloseq/Filtering phyloseq using prune().R
@@ -0,0 +1,55 @@
+# Load in packages:
+library(phyloseq)
+library(ape)
+library(tidyverse)
+
+# Load in the ms metadata,  feature table, taxonomy file, and rooted tree
+metafp <- "corrected_ms_metadata.tsv"
+meta <- read_delim(metafp, delim="\t")
+
+otufp <- "ms-mit-chlor-freq-filtered-table.txt"
+otu <- read_delim(file =otufp, delim="\t", skip=1)
+
+taxfp <- "ms-taxonomy.tsv"
+tax <- read_delim(taxfp, delim="\t")
+
+phylotreefp <- "ms-tree.nwk"
+phylotree <- read.tree(phylotreefp)
+
+# Adjust files to be read into a phyloseq object. Make the phyloseq object.
+# feature-table;
+otu_mat <- as.matrix(otu[,-1])
+rownames(otu_mat) <- otu$"#OTU ID"
+OTU <- otu_table(otu_mat, taxa_are_rows = TRUE)
+
+# metadata:
+meta_df <- as.data.frame(meta[,-1])
+rownames(meta_df) <- meta$"sample-id"
+META <- sample_data(meta_df)
+
+# taxonomy file:
+tax_mat <- tax %>%
+  select(-Confidence) %>%
+  separate(col=Taxon, sep="; ", into=c("Domain", "Phylum", "Class", "Order", "Family", "Genus", "Species")) %>%
+  as.matrix()
+tax_mat <- tax_mat[,-1]
+rownames(tax_mat) <- tax$"Feature ID"
+TAX <- tax_table(tax_mat)
+
+# create and save phyloseq object:
+unfiltered_ms_phyloseq <- phyloseq(OTU, META, TAX, phylotree)
+
+# removing duplicates from the phyloseq
+unique_metadata <- meta[!duplicated(meta$`sample-id`), ]
+
+# Update the phyloseq object with unique samples
+unique_ms_phyloseq <- prune_samples(rownames(sample_data(unfiltered_ms_phyloseq)) %in% unique_metadata$`sample-id`, unfiltered_ms_phyloseq)
+
+# Filtering out samples from individuals on probiotics and on special diets
+final_filtered_ms_phyloseq <- prune_samples(
+  sample_data(unique_ms_phyloseq)$probiotic == "0" & sample_data(unique_ms_phyloseq)$diet_no_special_needs == "1",
+  unique_ms_phyloseq
+)
+
+nsamples(final_filtered_ms_phyloseq)
+
