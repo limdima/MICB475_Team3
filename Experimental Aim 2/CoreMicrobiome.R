ead4eca164cd847208fe71d15d42ee94d6526648
Andrew Huang <you@example.com>
2024-11-05 10:09:17 -0800

    Modified core microbiome venn diagram to include control, and done on the newly filtered phyloseq object. Set prevalence to 33% and detection to 0.0001 to filter for ASVs that were only unique to 1 sample and any rare ASVs.
    Tried out a heatmap for fun to see what genus we are looking at, but not sure if the code is correct but it spits out a cool looking graph

--- Experimental Aim 2/CoreMicrobiome.R
+++ Experimental Aim 2/CoreMicrobiome.R
@@ -3,28 +3,31 @@ library(tidyverse)
 library(phyloseq)
 library(microbiome)
 library(ggVennDiagram)
+library(RColorBrewer)
 
 # load the phyloseq object
-load("phyloseq/ms_phyloseq.RData")
+load("phyloseq/final_filtered_ms_phyloseq.RData")
 
 # Convert object to relative abundance
-phyloseq_rel <- transform_sample_counts(ms_phyloseq, fun = function(x) x/sum(x))
+phyloseq_rel <- transform_sample_counts(final_filtered_ms_phyloseq, fun = function(x) x/sum(x))
 
 
 ### Subset dataset into treatment and control groups
 RRMS_stat <- subset_samples(phyloseq_rel, disease_course == "RRMS")
 SPMS_stat <- subset_samples(phyloseq_rel, disease_course == "SPMS")
 PPMS_stat<- subset_samples(phyloseq_rel, disease_course == "SPMS")
+control_stat <- subset_samples(phyloseq_rel, disease_course == "Control")
 
 ### Set a prevalence threshold and abundance threshold
-RRMS_ASVs <- core_members(RRMS_stat, detection=0, prevalence = 0)
-SPMS_ASVs <- core_members(SPMS_stat, detection=0, prevalence = 0)
-PPMS_ASVs <- core_members(PPMS_stat, detection=0, prevalence = 0)
-# Keep all ASVs including rare ASVs (detection = 0% relative abundance)
-# Find ASVs present in any samples (prevalence = 0), trying to tease out what is unique to each group
+RRMS_ASVs <- core_members(RRMS_stat, detection=0.0001, prevalence = 0.33)
+SPMS_ASVs <- core_members(SPMS_stat, detection=0.0001, prevalence = 0.33)
+PPMS_ASVs <- core_members(PPMS_stat, detection=0.0001, prevalence = 0.33)
+control_ASVs <- core_members(control_stat, detection=0.0001, prevalence = 0.33)
+# Exclude very rare ASVs (detection = 0.001% relative abundance)
+# Find ASVs present in 33% of samples (prevalence = 0.33), to account for outliers/ASVs unique to only 1 person
 
 ### Make a Venn-diagram
-venn_pd <- ggVennDiagram(x=list(RRMS = RRMS_ASVs, SPMS = SPMS_ASVs, PPMS = PPMS_ASVs)) +
+venn_pd <- ggVennDiagram(x=list(RRMS = RRMS_ASVs, SPMS = SPMS_ASVs, PPMS = PPMS_ASVs, Control = control_ASVs)) +
   scale_x_continuous(expand = expansion(mult = .2)) # this line helps fit the labels into the plot border
 
 venn_pd
@@ -32,9 +35,25 @@ venn_pd
 ### save venn diagram
 ggsave("Experimental Aim 2/venn_ms_groups.png", plot = venn_pd)
 
-# check what taxa we have
-# watch out the following can take a long time and the graph needs to be modified to fit everything
-#prune_taxa(PPMS_ASVs,ms_phyloseq) %>%
-#  plot_bar(fill = "Genus") +
-#  facet_wrap(.~ disease_course, scales = "free")
+
+# As a preliminary check, check what taxa we have by a heat map
+# (not 100% sure how the code works but copied from core microbiome tutorial websites)
+# https://microbiome.github.io/tutorials/CoremicrobiotaAmplicon.html
+
+prevalences <- seq(0.33, 1, .1) # set prevalence threshold from 0.33 to 1, in increments of 0.1  
+detections <- seq(0.0001, 0.05, 0.002) 
+# detection threshold from 0.0001 to get rid of low-abundance ASVs, 
+# up to 0.05 (to just view what is rare/common), in increments of 0.002
+
+phyloseq_rel.gen <- aggregate_taxa(phyloseq_rel, "Genus") # changes the ASVs to Genus names
+
+p.core <- plot_core(phyloseq_rel.gen, 
+                    plot.type = "heatmap", 
+                    colours = rev(brewer.pal(5, "Spectral")),
+                    prevalences = prevalences, 
+                    detections = detections, 
+                    min.prevalence = .33) + 
+  xlab("Detection Threshold (Relative Abundance (%))")
+
+print(p.core) 
 
